8a2095355df04fc10db3462a03947ff9d572eb9b
Jeff Bayntun <jeff.b@runbox.com>
2020-06-25 19:58:24 -0700

    Require player names, limit length.

--- src/screens/StartScreen.js
+++ src/screens/StartScreen.js
@@ -2,36 +2,56 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 const StartScreen = ({ navigation }) => {
-    const [player1, setPlayer1] = useState('');
-    const [player2, setPlayer2] = useState('');
+    const [player1, setPlayer1] = useState(null);
+    const [player2, setPlayer2] = useState(null);
 
   return (
     <View style={styles.container}>
+        <Text style={{fontSize: 24, margin: 20}}>Enter Player Names: </Text>
         <View style={styles.input}>
-            <Text style={styles.content}>Player 1: </Text>
             <TextInput
                 style={styles.content}
-                placeholder="Name"
+                placeholder="Player 1"
                 value={player1}
                 onChangeText={(text) => setPlayer1(text)}
                 autoCorrect={false}
+                autoFocus={true}
+                maxLength={12}
             />
         </View>
+        <View style={styles.errorHolder}>
+            {(player1 === '') ? <Text style={styles.error}>Player name can't be empty!</Text>:null}
+        </View>
         <View style={styles.input}>
-            <Text style={styles.content}>Player 2: </Text>
             <TextInput
                 style={styles.content}
-                placeholder="Name"
+                placeholder="Player 2"
                 value={player2}
                 onChangeText={(text) => setPlayer2(text)}
                 autoCorrect={false}
+                maxLength={12}
             />
         </View>
-        <Text></Text>
+        <View style={styles.errorHolder}>
+            {(player2 === '') ? <Text style={styles.error}>Player name can't be empty!</Text>:null}
+        </View>
         <Button
-            title="Let's Go!"
-            onPress={() => navigation.navigate('Local', {player1: player1, player2: player2})}
-            style={styles.button}
+            title="    Let's Go!    "
+            onPress={() => {
+                if(player1 && player2)
+                {
+                    navigation.navigate('Local', {player1: player1, player2: player2})
+                }
+                if(player1 == null)
+                {
+                    setPlayer1('');
+                }
+
+                if(player2 == null)
+                {
+                    setPlayer2('');
+                }
+            }}
         />
     </View>
   );
@@ -41,14 +61,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'gray',
-    alignItems: 'stretch',
+    alignItems: 'center',
     justifyContent: 'center',
   },
   input: {
     backgroundColor: '#F0EEEE',
     borderRadius: 5,
     marginHorizontal: 10,
-    marginTop: 15,
     padding: 5,
     flexDirection: 'row',
   },
@@ -58,10 +77,17 @@ const styles = StyleSheet.create({
     padding: 1,
     flex: 1,
   },
-  button:{
-    marginHorizontal: 15,
-    marginVertical: 30,
-  }
+  error: {
+      backgroundColor: '#ffc1cc',
+      width: '90%',
+      borderRadius: 5,
+      padding: 4,
+  },
+  errorHolder: {
+    height:'6%',
+    marginHorizontal: 10,
+    marginVertical: 6,
+    }
 });
 
 export default StartScreen;
